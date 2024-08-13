50262daf2d5516c755f156232c204f408a73347b
jaleverchen <jaleverchen@gmail.com>
2024-08-13 12:52:11 +0800

    feat(*): init

---
+++ src/test/java/com/example/springbootadventure/SpringbootAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootadventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdventureApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
