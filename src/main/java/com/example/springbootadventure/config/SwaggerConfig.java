ec2a436ef3ac859bb8fa6a2f97b4bf09a3caa6bb
ccdrm28 <jaleverchen@outlook.com>
2024-08-21 12:47:23 +0800

    feat(*): add swagger

--- src/main/java/com/example/springbootadventure/config/SwaggerConfig.java
+++ src/main/java/com/example/springbootadventure/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.example.springbootadventure.config;
 
-
 import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
